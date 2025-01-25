aeccaf84d0034fd0f5eda49b472a4c6c7e587d4b
ZachShack <zachary.shackleton@snhu.edu>
2025-01-25 10:39:17 -0500

    Module 3 completed

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,13 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+//del later
+//var header = JSON.parse(fs.readFileSync('./data/header.json','utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways'});
+    res.render('travel', { title: 'Travlr Getaways', trips});
 };
+//Module 3 shows its not best practice to run a JSON file everytime and we may need to adjust latr
 
 module.exports = {
     travel
